aae1ac9aa7f961741ed30441ffad66fc56666d2f
jayant@JJ <jayantjainofficial@gmail.com>
2022-04-30 21:44:02 +0530

    added code

--- src/main/java/com/patni/lld/Application.java
+++ src/main/java/com/patni/lld/Application.java
@@ -1,7 +1,11 @@
 package com.patni.lld;
 
+import com.patni.lld.cache.Cache;
+import com.patni.lld.cache.CacheFactory;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        CacheFactory<String, String> cacheFactory = new CacheFactory<>();
+        Cache cache = cacheFactory.defaultCache(100);
     }
 }
