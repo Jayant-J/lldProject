42cab19505e50c155117611281afbb78b22cc5a4
jayant@JJ <jayantjainofficial@gmail.com>
2022-04-30 21:45:29 +0530

    added code

--- src/main/java/com/patni/lld/Application.java
+++ src/main/java/com/patni/lld/Application.java
@@ -1,7 +1,24 @@
 package com.patni.lld;
 
+import com.patni.lld.client.ConsoleClient;
+import com.patni.lld.interaction.CommandHandler;
+import com.patni.lld.model.House;
+import com.patni.lld.service.ExpenseManagementService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        House house = new House();
+        ExpenseManagementService expenseManagementService = new ExpenseManagementService(house);
+        CommandHandler commandHandler = CommandHandler.init(expenseManagementService);
+        try {
+            ConsoleClient consoleClient = new ConsoleClient(new BufferedReader(new InputStreamReader(System.in)), commandHandler);
+            consoleClient.handleInput();
+        } catch (IOException ex) {
+            System.out.println("Something went wrong. Please try again!");
+        }
     }
 }
